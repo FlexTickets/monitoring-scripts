a02a5a4ffc5bc25cf2ca0fb2a8de4ac9ecf562d1
FlexTickets <F.Kolodyazhny@gmail.com>
2023-11-10 14:20:55 +0200

    Replace vmstat with top

--- checkCpuUsage.sh
+++ checkCpuUsage.sh
@@ -13,7 +13,7 @@ fi
 # Redirect stdout and stderr to syslog
 exec 1> >(logger -s -t $(basename $0)) 2>&1
 
-USAGE=$(vmstat 5 1 | tail -1 | awk '{print $13+$14}')
+USAGE=$(top -n 1 | grep "Cpu(s):" | awk '{print $2+$4}')
 
 if [[ -f ${FILE} ]]; then
 	THRESHOLD=$(head -1 ${FILE})
@@ -37,13 +37,13 @@ THRESHOLD=$1
 [ $# -eq 2 ]  && hostname=$2 || hostname=$(hostname)
 
 #echo "${THRESHOLD} ${lastUsage} ${ALARM} ${USAGE}"
-if [[ ${USAGE} -gt ${THRESHOLD} && ${lastUsage} -gt ${THRESHOLD} && ${ALARM} -eq 0 ]]; then
-	${scriptDir}/send2bot.sh "${hostname} High CPU Usage ALERT (${USAGE}%) $(ps aux | sort -nrk 3,3 | head -n 1)"
-	ALARM=1
+if (( $(echo "${USAGE} > ${THRESHOLD}" | bc -l) && $(echo "${lastUsage} > ${THRESHOLD}" | bc -l) && ${ALARM} == 0 )); then
+        ${scriptDir}/send2bot.sh "${hostname} High LA ALERT (${USAGE}) $(ps aux | sort -nrk 3,3 | head -n 1)"
+        ALARM=1
 fi
-if [[ ${USAGE} -lt ${THRESHOLD} && ${ALARM} -eq 1 ]]; then
-	${scriptDir}/send2bot.sh "${hostname} High CPU Usage OK (${USAGE}%)"
-	ALARM=0
+if (( $(echo "${USAGE} <= ${THRESHOLD}" | bc -l) && ${ALARM} == 1 )); then
+        ${scriptDir}/send2bot.sh "${hostname} High LA OK (${USAGE})"
+        ALARM=0
 fi
 
 echo "${USAGE} ${ALARM}" > ${FILE}
--- checkTlsExpiration.sh
+++ checkTlsExpiration.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -x
+#set -x
 set -uo pipefail
 
 DAYS=30
